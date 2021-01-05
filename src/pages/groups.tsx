d35cf776c19887cbc817e7a45c7a15de0d00cd5e
devkeita <keita.mywork@gmail.com>
2021-01-05 13:38:37 +0900

    add pages

---
+++ src/pages/groups.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Groups = () => {
+    return (
+        <div>
+            グループ一覧
+        </div>
+    );
+};
+
+export default Groups;
---
+++ src/pages/settings.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Settings = () => {
+    return (
+        <div>
+            設定
+        </div>
+    );
+};
+
+export default Settings;
